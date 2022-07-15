77f763bc2d932ea3a9bfee83f9ec2db694d70564
STRnick <nicksumin97@naver.com>
2022-07-15 09:48:00 +0900

    JSX tutorial #finish

---
+++ 1.basics/ex08/src/08/Content.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Clock from './Clock';
+
+function Content() {
+    return (
+        <Fragment>
+            <Clock />
+        </Fragment>
+    );
+}
+
+export default Content;
