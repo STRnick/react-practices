e419280b4d9d5bdaee21c9fb5f536cac551c0fa8
STRnick <nicksumin97@naver.com>
2022-07-15 11:22:55 +0900

    component-props #1

---
+++ 2.component/ex01/src/01/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Foodlist from './FoodList'
+
+class App extends Component {
+    render() {
+        return (
+            <div id='App'>
+                <Foodlist />
+            </div>
+        );
+    }
+}
+
+export default App;
