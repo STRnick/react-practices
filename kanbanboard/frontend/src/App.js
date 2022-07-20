56b1a0136697485fe99ebc4a472f828a786fb8d7
STRnick <nicksumin97@naver.com>
2022-07-20 10:31:50 +0900

    kanbanboard

---
+++ kanbanboard/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import KanbanBoard from './KanbanBoard';
+
+function App() { 
+    return (
+        <div id={'App'}>
+            <KanbanBoard />
+        </div>
+    );
+}
+
+export default App;
