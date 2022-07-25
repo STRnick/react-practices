207734a4c3338f72cb2b592c07fd072fe1b1534a
STRnick <nicksumin97@naver.com>
2022-07-25 10:19:08 +0900

    component lifecycle

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -1,33 +1,39 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import TaskList from "./TaskList";
 import Task from "./Task";
 import styles from './assets/scss/Card.scss';
 
 export default function Card({no, title, description, status, tasks}) {
-    const {title, description, tasks} = card;
+    const [showDetail, setShowDetail] = useState(false);
+    const styleSideColor = {
+        position: 'absolute',
+        zIndex: -1,
+        top: 0,
+        left: 0,
+        bottom: 0,
+        width: 3,
+        backgroundColor: status === 'ToDo' ? '#bd8D31' : (status === 'Doing' ? '#3a7e28' : '#222')
+    }
 
-  const [showDetail, setShowDetail] = useState(true);
-
-
-  return (
-    <div className={styles.Card}>
-      <div 
-        className={showDetail ? styles.Card__Title : [styles.Card__Title, styles.Card__Title__open].join(" ")}
-        onClick={() => setShowDetail(prevShowDetail => !prevShowDetail)} >
-        {title}
-      </div>
-      {
-      showDetail ? 
-      null :       
-      <div className={'Card__Details'}>
-        {description}
-        <TaskList tasks={tasks}/>
-      </div>
-      }
-
-    </div>
-  );
+    return (
+        <div className={styles.Card}>
+            <div style={styleSideColor}/>
+            <div
+                className={showDetail ? [styles.Card__Title, styles.Card__Title__Open].join(" ") : styles.Card__Title}
+                onClick={e => setShowDetail(!showDetail)}>
+                {title}
+            </div>
+            <div class='Card__Details'>
+              {description}
+              {
+                showDetail ?
+                    <TaskList tasks={tasks} /> : 
+                    null
+              }
+            </div> 
+        </div>
+    );
 }
 
 Card.propTypes = {
